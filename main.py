99f154f2a40ae7f4d8e050c9d63133be1c746df6
Pedro Peres <p.peresbjj@gmail.com>
2023-12-02 13:05:29 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2
+import os
+
+video_capture = cv2.VideoCapture('/Users/pedro/Desktop/untitled folder 3/335483_480p_.mp4')
+
+
+if not video_capture.isOpened():
+    print("Error: Couldn't open the video file.")
+    exit()
+
+frame_count = 0
+
+output_directory = '/Users/pedro/Desktop/untitled folder 3/frames'
+
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+while True:
+    # Read a frame from the video
+    ret, frame = video_capture.read()
+
+    if not ret:
+        break
+
+    frame_filename = os.path.join(output_directory, f'frame_{frame_count:04d}.png')
+    #print(f'Saving frame as: {frame_filename}')
+    cv2.imwrite(frame_filename, frame)
+
+    frame_count += 1
+#print(f'Output directory: {output_directory}')
+
+video_capture.release()
+
+#print(f'Frames extracted: {frame_count}')
